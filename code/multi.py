7ac54b0ff14ca9ed56dfa2c9f9ec3f55bb15f5c0
Mohamed Omar <mohamed222ahmed222@gmail.com>
2021-06-25 16:33:12 -0400

    cell filtering based on DAPI"

--- code/multi.py
+++ code/multi.py
@@ -24,10 +24,24 @@ import re
 import scipy.stats
 import batchglm.api as glm
 import diffxpy.api as de
+import bbknn
+
 #sc.set_figure_params(dpi=200, frameon=True, figsize=(20,15), format='png')
 
 ###################################################
+## Load data from Noolan
+NoolanData = pd.read_csv('./data/CRC_clusters_neighborhoods_markers.csv')
+NoolanData.shape
+NoolanData['ClusterName'].value_counts()
+NoolanData['groups'].value_counts()
+NoolanData['TMA_AB'].value_counts()
+
+NoolanData_groups = NoolanData[['File Name', 'groups', 'patients']]
+NoolanData_groups.rename(columns = {"File Name": "Region"}, inplace=True)
+
+#######################################################
 channelnames = pd.read_csv("data/channelNames.txt", header = None, dtype = str, low_memory=False)
+
 
 
 ## Load all images
@@ -183,16 +197,16 @@ adata_combined_All = ad.read_h5ad('./data/loomCombined_All.h5ad', chunk_size=100
 # Rename the variable names (channels) in the adata object
 adata_combined_All.var_names = channelnames[0]
 
+#adata_combined_All = adata_combined_All[adata_combined_All[: , 'HOECHST1'].X > 0, :]
 #adata_combined.obs['origin'] = re.sub("[^a-zA-Z0-9 \n\.]", "", str(adata_combined.obs['origin']))
 #adata_combined.obs['origin'] = re.sub("VectraSlidename", "", str(adata_combined.obs['origin']))
 #adata_combined.obs['origin'] = re.sub("\s+", "", str(adata_combined.obs['origin']))
-
 #adata_combined.obs['origin'] = re.sub("^slide .*", '', str(adata_combined.obs['origin']))
 
 
 #######################################
 ## Remove the empty and nuclear channels
-keep = ['CD44 - stroma', 'FOXP3 - regulatory T cells', 'CD8 - cytotoxic T cells', 'p53 - tumor suppressor', 'GATA3 - Th2 helper T cells', 'CD45 - hematopoietic cells', 'T-bet - Th1 cells', 'beta-catenin - Wnt signaling', 'HLA-DR - MHC-II', 'PD-L1 - checkpoint', 'Ki67 - proliferation', 'CD45RA - naive T cells', 'CD4 - T helper cells', 'CD21 - DCs', 'MUC-1 - epithelia', 'CD30 - costimulator', 'CD2 - T cells', 'Vimentin - cytoplasm', 'CD20 - B cells', 'LAG-3 - checkpoint', 'CD5 - T cells', 'IDO-1 - metabolism', 'Cytokeratin - epithelia', 'CD11b - macrophages', 'CD56 - NK cells', 'aSMA - smooth muscle', 'BCL-2 - apoptosis', 'CD25 - IL-2 Ra', 'CD11c - DCs', 'PD-1 - checkpoint', 'Granzyme B - cytotoxicity', 'EGFR - singling', 'VISTA - costimulator', 'CD15 - granulocytes', 'ICOS - costimulator', 'Synaptophysin - neuroendocrine', 'GFAP - nerves', 'CD7 - T cells', 'CD3 - T cells', 'Chromogranin A - neuroendocrine', 'CD163 - macrophages', 'CD57 - NK cells', 'CD45RO - memory cells', 'CD68 - macrophages', 'CD31 - vasculature', 'Podoplanin - lymphatics', 'CD34 - vasculature', 'CD38 - multifunctional', 'CD138 - plasma cells']
+keep = ['HOECHST1', 'CD44 - stroma', 'FOXP3 - regulatory T cells', 'CD8 - cytotoxic T cells', 'p53 - tumor suppressor', 'GATA3 - Th2 helper T cells', 'CD45 - hematopoietic cells', 'T-bet - Th1 cells', 'beta-catenin - Wnt signaling', 'HLA-DR - MHC-II', 'PD-L1 - checkpoint', 'Ki67 - proliferation', 'CD45RA - naive T cells', 'CD4 - T helper cells', 'CD21 - DCs', 'MUC-1 - epithelia', 'CD30 - costimulator', 'CD2 - T cells', 'Vimentin - cytoplasm', 'CD20 - B cells', 'LAG-3 - checkpoint', 'CD5 - T cells', 'IDO-1 - metabolism', 'Cytokeratin - epithelia', 'CD11b - macrophages', 'CD56 - NK cells', 'aSMA - smooth muscle', 'BCL-2 - apoptosis', 'CD25 - IL-2 Ra', 'CD11c - DCs', 'PD-1 - checkpoint', 'Granzyme B - cytotoxicity', 'EGFR - singling', 'VISTA - costimulator', 'CD15 - granulocytes', 'ICOS - costimulator', 'Synaptophysin - neuroendocrine', 'GFAP - nerves', 'CD7 - T cells', 'CD3 - T cells', 'Chromogranin A - neuroendocrine', 'CD163 - macrophages', 'CD57 - NK cells', 'CD45RO - memory cells', 'CD68 - macrophages', 'CD31 - vasculature', 'Podoplanin - lymphatics', 'CD34 - vasculature', 'CD38 - multifunctional', 'CD138 - plasma cells']
 
 adata_combined_All_t = adata_combined_All.transpose()
 adata_combined_All = adata_combined_All_t[np.isin(adata_combined_All.var.index, keep)].copy().transpose()
@@ -200,9 +214,329 @@ adata_combined_All.obs_names_make_unique()
 
 # Rename the markers
 #adata_combined_All.var_names = ['CD44_stroma', 'FOXP3_regTcells', 'CDX2_intestinalEpith', 'CD8', 'P53', 'GATA3_TH2', 'CD45_Hemato', 'Tbet_TH1', 'beta_Cat', 'HLA_DR', 'PD_L1', 'Ki67', 'CD45RA_naiveT', 'CD4', 'CD21_DCs', 'MUC1_epith', 'CD30_costimulator', 'CD2_Tcells', 'Vimentin', 'CD20_Bcells', 'LAG3_Checkpoint', 'NaKatpase_memb', 'CD5_Tcells', 'IDO1_metabolism', 'cytokeratin_epith', 'CD11b_macrophages', 'CD56_NK', 'aSMA_smoothMuscle', 'BCL2_apoptosis', 'CD25_IL2Ra', 'Collagen4_BasMemb', 'CD11c_DCs', 'PD1_checkpoint', 'GranzymeB_Cytotoxicity', 'EGFR', 'VISTA_Costimulator', 'CD15_granulocytes', 'CD194_CCR4ChemokineR', 'ICOS_Costimulator', 'MMP9_MatrixMetalloProt', 'Synapto_NE', 'CD71_TransferrinR', 'GFAP_nerves', 'CD7_Tcells', 'CD3_Tcells', 'ChromograninA_NE', 'CD163_macrophages', 'CD57_NK', 'CD45RO_memoryCells', 'CD68_macrophages', 'CD31_vasculature', 'Podoplanin_lymphatics', 'CD34_vasculature', 'CD38_multi', 'CD138_plasmaCells', 'MMP12_MatrixMetellaProt']
-adata_combined_All.var_names = ['CD44-stroma', 'FOXP3-Treg', 'CD8', 'p53', 'GATA3-Th2', 'CD45-hematopoietic', 'T-bet-Th1', 'beta-cat', 'HLA-DR', 'PD-L1', 'Ki67', 'CD45RA-naiveT', 'CD4', 'CD21-DCs', 'MUC-1-epithelia', 'CD30-costimulator', 'CD2-Tcells', 'Vimentin', 'CD20', 'LAG-3-checkpoint', 'CD5-Tcells', 'IDO-1-metabolism', 'Cytokeratin', 'CD11b-macrophages', 'CD56-NK cells', 'aSMA-smooth muscle', 'BCL-2-apoptosis', 'CD25-IL-2Ra', 'CD11c-DCs', 'PD-1-checkpoint', 'Granzyme B-cytotoxicity', 'EGFR', 'VISTA-costimulator', 'CD15-granulocytes', 'ICOS-costimulator', 'Synaptophysin-neuroendocrine', 'GFAP-nerves', 'CD7-Tcells', 'CD3-Tcells', 'ChromograninA-neuroendocrine', 'CD163-macrophages', 'CD57-NK cells', 'CD45RO-memory cells', 'CD68-macrophages', 'CD31-vasculature', 'Podoplanin-lymphatics', 'CD34-vasculature', 'CD38-multifunctional', 'CD138-plasma cells']
+adata_combined_All.var_names = ['HOECHST1', 'CD44-stroma', 'FOXP3-Treg', 'CD8', 'p53', 'GATA3-Th2', 'CD45-hematopoietic', 'T-bet-Th1', 'beta-cat', 'HLA-DR', 'PD-L1', 'Ki67', 'CD45RA-naiveT', 'CD4', 'CD21-DCs', 'MUC-1-epithelia', 'CD30-costimulator', 'CD2-Tcells', 'Vimentin', 'CD20', 'LAG-3-checkpoint', 'CD5-Tcells', 'IDO-1-metabolism', 'Cytokeratin', 'CD11b-macrophages', 'CD56-NK cells', 'aSMA-smooth muscle', 'BCL-2-apoptosis', 'CD25-IL-2Ra', 'CD11c-DCs', 'PD-1-checkpoint', 'Granzyme B-cytotoxicity', 'EGFR', 'VISTA-costimulator', 'CD15-granulocytes', 'ICOS-costimulator', 'Synaptophysin-neuroendocrine', 'GFAP-nerves', 'CD7-Tcells', 'CD3-Tcells', 'ChromograninA-neuroendocrine', 'CD163-macrophages', 'CD57-NK cells', 'CD45RO-memory cells', 'CD68-macrophages', 'CD31-vasculature', 'Podoplanin-lymphatics', 'CD34-vasculature', 'CD38-multifunctional', 'CD138-plasma cells']
 
-# Check
+sc.pl.violin(adata_combined_All, keys=['HOECHST1'], save='DAPI.png')
+
+
+############################
+## Add patients and groups info
+NoolanData_patients = NoolanData_groups.copy()
+NoolanData_groups = NoolanData_groups.groupby(['Region', 'groups']).size().unstack(fill_value=0)
+
+## Annotation dict for patients
+regions_to_patients = dict(
+reg001_A = '1',
+reg001_B = '1',
+reg002_A = '1',
+reg002_B = '1',
+reg003_A = '2',
+reg003_B = '2',
+reg004_A = '2',
+reg004_B = '2',
+reg005_A = '3',
+reg005_B = '3',
+reg006_A = '3',
+reg006_B = '3',
+reg007_A = '4',
+reg007_B = '4',
+reg008_A = '4',
+reg008_B = '4',
+reg009_A = '5',
+reg009_B = '5',
+reg010_A = '5',
+reg010_B = '5',
+reg011_A = '6',
+reg011_B = '6',
+reg012_A = '6',
+reg012_B = '6',
+reg013_A = '7',
+reg013_B = '7',
+reg014_A = '7',
+reg014_B = '7',
+reg015_A = '8',
+reg015_B = '8',
+reg016_A = '8',
+reg016_B = '8',
+reg017_A = '9',
+reg017_B = '9',
+reg018_A = '9',
+reg018_B = '9',
+reg019_A = '10',
+reg019_B = '10',
+reg020_A = '10',
+reg020_B = '10',
+reg021_A = '11',
+reg021_B = '11',
+reg022_A = '11',
+reg022_B = '11',
+reg023_A = '12',
+reg023_B = '12',
+reg024_A = '12',
+reg024_B = '12',
+reg025_A = '13',
+reg025_B = '13',
+reg026_A = '13',
+reg026_B = '13',
+reg027_A = '14',
+reg027_B = '14',
+reg028_A = '14',
+reg028_B = '14',
+reg029_A = '15',
+reg029_B = '15',
+reg030_A = '15',
+reg030_B = '15',
+reg031_A = '16',
+reg031_B = '16',
+reg032_A = '16',
+reg032_B = '16',
+reg033_A = '17',
+reg033_B = '17',
+reg034_A = '17',
+reg034_B = '17',
+reg035_A = '18',
+reg035_B = '18',
+reg036_A = '18',
+reg036_B = '18',
+reg037_A = '19',
+reg037_B = '19',
+reg038_A = '19',
+reg038_B = '19',
+reg039_A = '20',
+reg039_B = '20',
+reg040_A = '20',
+reg040_B = '20',
+reg041_A = '21',
+reg041_B = '21',
+reg042_A = '21',
+reg042_B = '21',
+reg043_A = '22',
+reg043_B = '22',
+reg044_A = '22',
+reg044_B = '22',
+reg045_A = '23',
+reg045_B = '23',
+reg046_A = '23',
+reg046_B = '23',
+reg047_A = '24',
+reg047_B = '24',
+reg048_A = '24',
+reg048_B = '24',
+reg049_A = '25',
+reg049_B = '25',
+reg050_A = '25',
+reg050_B = '25',
+reg051_A = '26',
+reg051_B = '26',
+reg052_A = '26',
+reg052_B = '26',
+reg053_A = '27',
+reg053_B = '27',
+reg054_A = '27',
+reg054_B = '27',
+reg055_A = '28',
+reg055_B = '28',
+reg056_A = '28',
+reg056_B = '28',
+reg057_A = '29',
+reg057_B = '29',
+reg058_A = '29',
+reg058_B = '29',
+reg059_A = '30',
+reg059_B = '30',
+reg060_A = '30',
+reg060_B = '30',
+reg061_A = '31',
+reg061_B = '31',
+reg062_A = '31',
+reg062_B = '31',
+reg063_A = '32',
+reg063_B = '32',
+reg064_A = '32',
+reg064_B = '32',
+reg065_A = '33',
+reg065_B = '33',
+reg066_A = '33',
+reg066_B = '33',
+reg067_A = '34',
+reg067_B = '34',
+reg068_A = '34',
+reg068_B = '34',
+reg069_A = '35',
+reg069_B = '35',
+reg070_A = '35',
+reg070_B = '35'
+)
+
+
+## Annotation dict for groups
+regions_to_groups = dict(
+reg001_A = 'CLR',
+reg001_B = 'CLR',
+reg002_A = 'CLR',
+reg002_B = 'CLR',
+reg003_A = 'DII',
+reg003_B = 'DII',
+reg004_A = 'DII',
+reg004_B = 'DII',
+reg005_A = 'DII',
+reg005_B = 'DII',
+reg006_A = 'DII',
+reg006_B = 'DII',
+reg007_A = 'DII',
+reg007_B = 'DII',
+reg008_A = 'DII',
+reg008_B = 'DII',
+reg009_A = 'DII',
+reg009_B = 'DII',
+reg010_A = 'DII',
+reg010_B = 'DII',
+reg011_A = 'CLR',
+reg011_B = 'CLR',
+reg012_A = 'CLR',
+reg012_B = 'CLR',
+reg013_A = 'DII',
+reg013_B = 'DII',
+reg014_A = 'DII',
+reg014_B = 'DII',
+reg015_A = 'DII',
+reg015_B = 'DII',
+reg016_A = 'DII',
+reg016_B = 'DII',
+reg017_A = 'DII',
+reg017_B = 'DII',
+reg018_A = 'DII',
+reg018_B = 'DII',
+reg019_A = 'CLR',
+reg019_B = 'CLR',
+reg020_A = 'CLR',
+reg020_B = 'CLR',
+reg021_A = 'CLR',
+reg021_B = 'CLR',
+reg022_A = 'CLR',
+reg022_B = 'CLR',
+reg023_A = 'CLR',
+reg023_B = 'CLR',
+reg024_A = 'CLR',
+reg024_B = 'CLR',
+reg025_A = 'CLR',
+reg025_B = 'CLR',
+reg026_A = 'CLR',
+reg026_B = 'CLR',
+reg027_A = 'DII',
+reg027_B = 'DII',
+reg028_A = 'DII',
+reg028_B = 'DII',
+reg029_A = 'DII',
+reg029_B = 'DII',
+reg030_A = 'DII',
+reg030_B = 'DII',
+reg031_A = 'DII',
+reg031_B = 'DII',
+reg032_A = 'DII',
+reg032_B = 'DII',
+reg033_A = 'CLR',
+reg033_B = 'CLR',
+reg034_A = 'CLR',
+reg034_B = 'CLR',
+reg035_A = 'DII',
+reg035_B = 'DII',
+reg036_A = 'DII',
+reg036_B = 'DII',
+reg037_A = 'CLR',
+reg037_B = 'CLR',
+reg038_A = 'CLR',
+reg038_B = 'CLR',
+reg039_A = 'CLR',
+reg039_B = 'CLR',
+reg040_A = 'CLR',
+reg040_B = 'CLR',
+reg041_A = 'CLR',
+reg041_B = 'CLR',
+reg042_A = 'CLR',
+reg042_B = 'CLR',
+reg043_A = 'DII',
+reg043_B = 'DII',
+reg044_A = 'DII',
+reg044_B = 'DII',
+reg045_A = 'DII',
+reg045_B = 'DII',
+reg046_A = 'DII',
+reg046_B = 'DII',
+reg047_A = 'CLR',
+reg047_B = 'CLR',
+reg048_A = 'CLR',
+reg048_B = 'CLR',
+reg049_A = 'DII',
+reg049_B = 'DII',
+reg050_A = 'DII',
+reg050_B = 'DII',
+reg051_A = 'DII',
+reg051_B = 'DII',
+reg052_A = 'DII',
+reg052_B = 'DII',
+reg053_A = 'DII',
+reg053_B = 'DII',
+reg054_A = 'DII',
+reg054_B = 'DII',
+reg055_A = 'CLR',
+reg055_B = 'CLR',
+reg056_A = 'CLR',
+reg056_B = 'CLR',
+reg057_A = 'CLR',
+reg057_B = 'CLR',
+reg058_A = 'CLR',
+reg058_B = 'CLR',
+reg059_A = 'DII',
+reg059_B = 'DII',
+reg060_A = 'DII',
+reg060_B = 'DII',
+reg061_A = 'DII',
+reg061_B = 'DII',
+reg062_A = 'DII',
+reg062_B = 'DII',
+reg063_A = 'CLR',
+reg063_B = 'CLR',
+reg064_A = 'CLR',
+reg064_B = 'CLR',
+reg065_A = 'CLR',
+reg065_B = 'CLR',
+reg066_A = 'CLR',
+reg066_B = 'CLR',
+reg067_A = 'CLR',
+reg067_B = 'CLR',
+reg068_A = 'CLR',
+reg068_B = 'CLR',
+reg069_A = 'CLR',
+reg069_B = 'CLR',
+reg070_A = 'CLR',
+reg070_B = 'CLR'
+)
+
+
+# Map the values
+adata_combined_All.obs['patients'] = (
+    adata_combined_All.obs['Region']
+    .map(regions_to_patients)
+    .astype('category')
+)
+
+adata_combined_All.obs['groups'] = (
+    adata_combined_All.obs['Region']
+    .map(regions_to_groups)
+    .astype('category')
+)
+
+
+
+## Sanity check
+adata_combined_All.obs['Region'].value_counts()
+adata_combined_All.obs['patients'].value_counts()
+adata_combined_All.obs['groups'].value_counts()
+
+#######
+##############
+# Check the count matrix
 CountMatrix = adata_combined_All.to_df()
 np.isnan(CountMatrix).sum()
 CountMatrix.isnull().sum()
@@ -213,7 +547,6 @@ CountMatrix
 
 ################################################
 ## Scanpy workflow
-
 #sc.pl.violin(adata_combined_All, keys = ['0','24', '29', '33', '60'])
 
 #sc.pl.highest_expr_genes(adata_combined_All, n_top=5, )
@@ -226,32 +559,53 @@ CountMatrix
 
 # Scaling
 sc.pp.log1p(adata_combined_All)
-sc.pp.scale(adata_combined_All, zero_center=False)
+sc.pp.scale(adata_combined_All, zero_center=True, max_value=10)
+sc.pl.violin(adata_combined_All, keys=['HOECHST1'], save='DAPI_scaled.png')
 
-# PCA
-sc.tl.pca(adata_combined_All, svd_solver='arpack')
-#sc.pl.pca_variance_ratio(adata_combined_All, log=True, n_pcs = 30)
+# Filter cells with DAPI expression < 0
+adata_combined_All = adata_combined_All[adata_combined_All[: , 'HOECHST1'].X > 0, :]
 
-sc.external.pp.harmony_integrate(adata_combined_All, 'TMA')
+# PCA and batch correction
+#bbknn.ridge_regression(adata_combined_All, batch_key=['Region'], confounder_key=['patients'])
+sc.tl.pca(adata_combined_All)
+bbknn.bbknn(adata_combined_All, batch_key='Region', neighbors_within_batch = 15, n_pcs = 30)
 
-sc.pl.embedding(adata_combined_All, basis='X_pca_harmony', color=['TMA'])
-sc.pl.embedding(adata_combined_All, basis='X_pca', color=['TMA'])
+# compute Umap embedding
+sc.tl.umap(adata_combined_All)
+
+# louvain clustering
+sc.tl.louvain(adata_combined_All, resolution = 0.5)
+adata_combined_All.obs['louvain'].value_counts()
+
+# Save results for DE with MAST
+adata_combined_All.write(filename='./data/loomCombined_Louvain.h5ad')
+
+
+
+
+#sc.external.pp.harmony_integrate(adata_combined_All, 'TMA')
+
+# Plot PCA embedding
+#sc.pl.embedding(adata_combined_All, basis='X_pca_harmony', color=['TMA'])
+#sc.pl.embedding(adata_combined_All, basis='X_pca', color=['TMA'])
 
 # Compute neighbors and embedding
 #sc.pp.neighbors(adata_combined_All, n_neighbors=15, n_pcs=30)
-sc.pp.neighbors(adata_combined_All, n_neighbors=15, n_pcs=30, use_rep='X_pca_harmony')
-sc.tl.umap(adata_combined_All)
+#sc.pp.neighbors(adata_combined_All, n_neighbors=15, n_pcs=30, use_rep='X_pca_harmony')
+#sc.tl.umap(adata_combined_All)
 
+# Plot UMAP embedding
 sc.pl.umap(adata_combined_All, color=['Region'])
 sc.pl.umap(adata_combined_All, color=['TMA'])
+sc.pl.umap(adata_combined_All, color=['patients'])
+sc.pl.umap(adata_combined_All, color=['groups'])
+sc.pl.umap(adata_combined_All, color=['louvain'])
 
 # Clustering (Leiden)
 #sc.tl.leiden(adata_combined_All, resolution = 0.3)
 #adata_combined_All.obs['leiden'].value_counts()
 
-# Clustering (Louvain)
-sc.tl.louvain(adata_combined_All, resolution = 0.5)
-adata_combined_All.obs['louvain'].value_counts()
+
 
 # Plot umap
 #with rc_context({'figure.figsize': (10, 10)}):
@@ -455,6 +809,8 @@ adata_combined_All.obs['cell_types'] = (
     .map(old_to_new)
     .astype('category')
 )
+
+
 ##############################################
 import squidpy as sq
 sc.set_figure_params(dpi=200, frameon= True, figsize=(15,15), format='png', fontsize=12)
@@ -475,14 +831,6 @@ plt.show()
 
 
 ###################################
-## Load data from Noolan
-NoolanData = pd.read_csv('./data/CRC_clusters_neighborhoods_markers.csv')
-NoolanData.shape
-NoolanData['ClusterName'].value_counts()
-NoolanData['groups'].value_counts()
-NoolanData['TMA_AB'].value_counts()
-
-
 ## Our data using pathml
 LoomData = adata_combined_All.to_df()
 LoomData['CellID'] = np.linspace(0, 406958, num= 406958).astype(int)
@@ -506,3 +854,6 @@ loomDataAnn.to_csv('./data/CRC_pathml.csv')
 
 X = pd.read_csv('./data/CRC_pathml.csv')
 len(X['cell_types'].unique())
+
+
+#############################
